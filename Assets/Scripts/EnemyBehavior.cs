e6aa84ae178aa45951fd6d27b6703d9da93d7d20
Cuvette <franrizzo@hotmail.fr>
2015-03-04 18:02:32 +0100

    comportement enemi

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+	
+	NavMeshAgent nav;
+
+	void Awake()
+	{
+
+		nav = GetComponent<NavMeshAgent> ();
+
+
+	}
+	void Update()
+	{
+		GameObject[] eggs = GameObject.FindGameObjectsWithTag ("Egg");
+		GameObject target = null;
+		foreach (GameObject egg in eggs) {
+			if (target == null || Vector3.Distance(target.transform.position,transform.position)>Vector3.Distance(egg.transform.position, transform.position))
+				target = egg;
+		}
+		if (target != null)
+		
+			nav.SetDestination(target.transform.position);
+	}
+
+
+		}
