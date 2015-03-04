69c1c7756068cd665957d9575d7473f6ef61a4d8
Cuvette <franrizzo@hotmail.fr>
2015-03-04 15:12:42 +0100

    Camera + mouvement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 6f;
+	Rigidbody playerRigidbody;
+	float camRaylength = 100f;
+	Vector3 movement;
+
+	void Awake()
+	{
+		playerRigidbody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+		Turning ();
+		//Animating()
+
+	}
+	void Move(float h, float v)
+	{
+		movement.Set (h, 0f, v);
+		movement = movement.normalized * speed * Time.fixedDeltaTime;
+		playerRigidbody.MovePosition (transform.position + movement);
+
+	}
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit floorHit;
+
+		if (Physics.Raycast (camRay, out floorHit, camRaylength))
+		{
+			Vector3 playertoMouse = floorHit.point - transform.position;
+
+			playertoMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation (playertoMouse);
+			playerRigidbody.MoveRotation (newRotation);
+		}
+	}
+
+}
